abd07bf0f5b7526424516f9121f13425c1b3f87e
MelchiorDahrk <47513579+MelchiorDahrk@users.noreply.github.com>
2021-05-21 14:23:09 -0400

    Crated WeaponSheathing interop

---
+++ 00 Core/MWSE/mods/OAAB/WeaponSheathing/main.lua
@@ -0,0 +1,11 @@
+local config = include("weaponSheathing.config")
+
+if config then
+    config.blocked["ab_w_bottlebroken"] = true
+    config.blocked["ab_w_cookknifebone"] = true
+    config.blocked["ab_w_cookknifebread"] = true
+    config.blocked["ab_w_cookknifechef"] = true
+	config.blocked["ab_w_cookknifecleave"] = true
+	config.blocked["ab_w_cookknifedinner"] = true
+	config.blocked["ab_w_markingknife"] = true
+end
