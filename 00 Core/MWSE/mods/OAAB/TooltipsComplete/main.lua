a04e10077fa9ea67c2ea358e81c923b52a2035cf
Reizeron <40732908+Reizeron@users.noreply.github.com>
2021-08-08 18:27:12 +0300

    Added Sillapi drink and a Tooltips Complete interop

---
+++ 00 Core/MWSE/mods/OAAB/TooltipsComplete/main.lua
@@ -0,0 +1,12 @@
+local tooltipsComplete = include("Tooltips Complete.interop")
+local tooltipData = {
+    { id = "ab_dri_sillapi", description = "Sillapi is an egg miner's drink, sometimes used as a medicine as it can dull the pain and induce tranquil sleep. It's brewed from various kwama products, often right in the mines, and traditionally stored in a jug made of a treated kwama egg.", itemType = "ingredients" }
+}
+local function initialized()
+    if tooltipsComplete then
+        for _, data in ipairs(tooltipData) do
+            tooltipsComplete.addTooltip(data.id, data.description, data.itemType)
+        end
+    end
+end
+event.register("initialized", initialized)
