de46a8f49c5f1cd1972da96ee27d63dff13034f3
Reizeron <40732908+Reizeron@users.noreply.github.com>
2021-08-10 19:41:14 +0300

    OAAB The Shitty Update
    Added several dung items/objects

--- 00 Core/MWSE/mods/OAAB/TooltipsComplete/main.lua
+++ 00 Core/MWSE/mods/OAAB/TooltipsComplete/main.lua
@@ -1,6 +1,7 @@
 local tooltipsComplete = include("Tooltips Complete.interop")
 local tooltipData = {
-    { id = "ab_dri_sillapi", description = "Sillapi is an egg miner's drink, sometimes used as a medicine as it can dull the pain and induce tranquil sleep. It's brewed from various kwama products, often right in the mines, and traditionally stored in a jug made of a treated kwama egg.", itemType = "ingredients" }
+    { id = "ab_dri_sillapi", description = "Sillapi is an egg miner's drink, sometimes used as a medicine as it can dull the pain and induce tranquil sleep. It's brewed from various kwama products, often right in the mines, and traditionally stored in a jug made of a treated kwama egg.", itemType = "ingredients" },
+	{ id = "ab_ingcrea_dungcake", description = "A flat, crumbly patty made of dried manure. Has a slight musky smell.", itemType = "ingredients" }
 }
 local function initialized()
     if tooltipsComplete then
