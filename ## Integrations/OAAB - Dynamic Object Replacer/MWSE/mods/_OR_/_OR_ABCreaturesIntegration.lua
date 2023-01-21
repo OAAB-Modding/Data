16b8a5cca9070c31eaa7fc64f8c9b0652bd5b232
MelchiorDahrk <47513579+MelchiorDahrk@users.noreply.github.com>
2023-01-20 23:05:13 -0500

    OAAB Dynamic Object Replacer

---
+++ ## Integrations/OAAB - Dynamic Object Replacer/MWSE/mods/_OR_/_OR_ABCreaturesIntegration.lua
@@ -0,0 +1,96 @@
+objectsToReplace = {
+    objectToReplace0 = {
+		oldObject = "dwarven ghost",
+		newObjects = {
+			newObject0 = {
+				newObject = "AB_Und_DwrvSpectreF",
+				replaceChance = 0.25,
+			},
+			newObject1 = {
+				newObject = "AB_Und_DwrvSpectreWep",
+				replaceChance = 0.25,
+			},
+			newObject2 = {
+				newObject = "AB_Und_DwrvSpectreWepF",
+				replaceChance = 0.25,
+			}
+		}
+	},
+	objectToReplace1 = {
+		oldObject = "scamp",
+		newObjects = {
+			newObject0 = {
+				newObject = "AB_Dae_Herne",
+					replaceChance = 0.20,
+					},
+				}
+			},
+	objectToReplace2 = {
+		oldObject = "golden saint",
+		newObjects = {
+			newObject0 = {
+				newObject = "AB_Dae_DarkSeducer",
+					replaceChance = 0.50,
+					},
+				}
+			},
+	objectToReplace3 = {
+		oldObject = "atronach_storm",
+		newObjects = {
+			newObject0 = {
+				newObject = "AB_Dae_AtroStormMonarch",
+					replaceChance = 0.05,
+					},
+				}
+			},
+	objectToReplace4 = {
+		oldObject = "atronach_flame",
+		newObjects = {
+			newObject0 = {
+				newObject = "AB_Dae_AtronachIron",
+					replaceChance = 0.15,
+					},
+				}
+			},
+	objectToReplace5 = {
+		oldObject = "slaughterfish",
+		newObjects = {
+			newObject0 = {
+				newObject = "AB_Fau_SfishBlind",
+					replaceChance = 0.10,
+					},
+				}
+			},
+	objectToReplace6 = {
+		oldObject = "Slaughterfish_Small",
+		newObjects = {
+			newObject0 = {
+				newObject = "AB_Fau_Sturgeon",
+					replaceChance = 0.10,
+					},
+			newObject1 = {
+				newObject = "AB_Fau_SturgeonAncient",
+					replaceChance = 0.05,
+					},
+				}
+			},
+	objectToReplace7 = {
+		oldObject = "scrib",
+		newObjects = {
+			newObject0 = {
+				newObject = "AB_Fau_KwamaGrubber",
+					replaceChance = 0.10,
+					},
+				}
+			},
+	objectToReplace8 = {
+		oldObject = "kwama worker",
+		newObjects = {
+			newObject0 = {
+				newObject = "AB_Fau_KwamaGrubber",
+					replaceChance = 0.10,
+					},
+				}
+			},
+		}
+return objectsToReplace
---
+++ ## Integrations/OAAB - Dynamic Object Replacer/MWSE/mods/_OR_/_OR_ABGemIntegration.lua
@@ -0,0 +1,79 @@
+objectsToReplace = {
+    objectToReplace0 = {
+		oldObject = "ingred_pearl_01",
+		newObjects = {
+			newObject0 = {
+				newObject = "AB_IngMine_GoldPearl_01",
+				replaceChance = 0.10,
+			},
+			newObject1 = {
+				newObject = "AB_IngMine_BlackPearl_01",
+				replaceChance = 0.05,
+			},
+		}
+	},
+    objectToReplace1 = {
+		oldObject = "ingred_diamond_01",
+		newObjects = {
+			newObject0 = {
+				newObject = "AB_IngMine_BlueDiamond",
+				replaceChance = 0.15,
+			},
+			newObject1 = {
+				newObject = "AB_IngMine_RedDiamond",
+				replaceChance = 0.15,
+			},
+		}
+	},
+    objectToReplace2 = {
+		oldObject = "ingred_ruby_01",
+		newObjects = {
+			newObject0 = {
+				newObject = "AB_IngMine_Amethyst_01",
+				replaceChance = 0.10,
+			},
+			newObject1 = {
+				newObject = "AB_IngMine_Diopside_01",
+				replaceChance = 0.05,
+			},
+			newObject2 = {
+				newObject = "AB_IngMine_Sapphire_01",
+				replaceChance = 0.10,
+			},
+			newObject3 = {
+				newObject = "AB_IngMine_Firejade_01",
+				replaceChance = 0.10,
+			},
+			newObject4 = {
+				newObject = "AB_IngMine_Amber_01",
+				replaceChance = 0.05,
+			},
+		}
+	},
+    objectToReplace3 = {
+		oldObject = "ingred_emerald_01",
+		newObjects = {
+			newObject0 = {
+			newObject = "AB_IngMine_BlackTourmaline_01",
+				replaceChance = 0.10,
+			},
+			newObject1 = {
+				newObject = "AB_IngMine_Tourmaline_01",
+				replaceChance = 0.10,
+			},
+			newObject2 = {
+				newObject = "AB_IngMine_Topaz_01",
+				replaceChance = 0.10,
+			},
+			newObject3 = {
+				newObject = "AB_IngMine_Peridot_01",
+				replaceChance = 0.07,
+			},
+			newObject4 = {
+				newObject = "AB_IngMine_Garnet_01",
+				replaceChance = 0.07,
+			},
+		}
+	},
+		}
+return objectsToReplace
