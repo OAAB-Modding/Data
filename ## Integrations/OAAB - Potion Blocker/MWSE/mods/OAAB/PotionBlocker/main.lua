5f26d33cfb3ed4572a153c644470f0b75a1f9590
MelchiorDahrk <47513579+MelchiorDahrk@users.noreply.github.com>
2023-03-09 07:49:01 -0500

    Removed integrations which are bundled by default

--- ## Integrations/OAAB - Potion Blocker/MWSE/mods/OAAB/PotionBlocker/main.lua
+++
@@ -1,7 +0,0 @@
-local function blockPotionEquipEvent(e)
-    if (e.item.id:find("^AB_eff_")) then
-        e.claim = true
-    end
-end
-event.register("equip", blockPotionEquipEvent, {priority = 1000})
-event.register("equipped", blockPotionEquipEvent, {priority = 1000})
