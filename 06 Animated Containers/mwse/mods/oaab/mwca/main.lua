b48136bd5c5328593529d6773c03075a30dc64f6
Reizeron <40732908+Reizeron@users.noreply.github.com>
2023-07-16 19:36:49 +0300

    Added a new common hutch

--- 06 Animated Containers/mwse/mods/oaab/mwca/main.lua
+++ 06 Animated Containers/mwse/mods/oaab/mwca/main.lua
@@ -25,7 +25,9 @@ if mwca then
 	mwca["oaab\\o\\comrchhutch.nif"] = {"OAAB\\o\\comrchhutchac.nif", 1, 2, 0.5, 0.5, "AC_drawer_open", "AC_drawer_close", 0};
 	mwca["oaab\\o\\comrchdesk.nif"] = {"OAAB\\o\\comrchdeskac.nif", 1, 2, 0.5, 0.5, "AC_drawer_de_open", "AC_drawer_de_close", 0};
 	mwca["oaab\\o\\comrchdesk2.nif"] = {"OAAB\\o\\comrchdesk2ac.nif", 1, 2, 0.5, 0.5, "AC_drawer_de_open", "AC_drawer_de_close", 0};
-	mwca["oaab\\o\\comdrawers.nif"] = {"OAAB\\o\\comdrawersac.nif", 1, 2, 0.5, 0.5, "AC_drawer_open", "AC_drawer_close", 0}
+	mwca["oaab\\o\\comdrawers.nif"] = {"OAAB\\o\\comdrawersac.nif", 1, 2, 0.5, 0.5, "AC_drawer_open", "AC_drawer_close", 0};
+	mwca["oaab\\o\\comhutch.nif"] = {"OAAB\\o\\comhutchac.nif", 1, 2, 0.5, 0.5, "AC_closet_open", "AC_closet_close", 0};
+	mwca["oaab\\o\\comhutchopen.nif"] = {"OAAB\\o\\comhutchopenac.nif", 1, 2, 0.5, 0.5, "AC_closet_open", "AC_closet_close", 0}
 	
 	
 end
