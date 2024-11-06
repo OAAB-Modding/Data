3ff3f4184863d13704c62bc13ef07106ae9819a0
MelchiorDahrk <47513579+MelchiorDahrk@users.noreply.github.com>
2024-11-06 17:03:22 -0500

    Merged fixed sound gen

--- Developer Tools/merge_to_master.bat
+++ Developer Tools/merge_to_master.bat
@@ -1,4 +1,4 @@
 @echo off
 mklink /h "OAAB_Data.esm" "..\\00 Core\\OAAB_Data.esm"
-.\\merge_to_master.exe %1 "..\\00 Core\\OAAB_Data.esm" --overwrite --remove-deleted
+.\\merge_to_master.exe %1 "OAAB_Data.esm" --overwrite --remove-deleted
 pause
