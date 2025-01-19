2bcd970b378a1a52c38dc3efdde5fe0622a415aa
MelchiorDahrk <47513579+MelchiorDahrk@users.noreply.github.com>
2025-01-19 10:30:49 -0500

    Update m2m

--- Developer Tools/merge_to_master.bat
+++ Developer Tools/merge_to_master.bat
@@ -1,4 +1,5 @@
 @echo off
-mklink /h "OAAB_Data.esm" "..\\00 Core\\OAAB_Data.esm"
-.\\merge_to_master.exe %1 "OAAB_Data.esm" --overwrite --remove-deleted
+copy /y "..\\00 Core\\OAAB_Data.esm" "OAAB_Data.esm"
+.\\merge_to_master.exe %1 "OAAB_Data.esm" --overwrite --remove-deleted --apply-moved-references
+move /y "OAAB_Data.esm" "..\\00 Core\\OAAB_Data.esm"
 pause
